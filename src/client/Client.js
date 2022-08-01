e0f273f7212cb7a0e5cb3796c508272b00232faf
KanakaNakazawa <97468385+KanakaNakazawa@users.noreply.github.com>
2022-08-01 17:07:27 +0700

    Create Client.js

---
+++ src/client/Client.js
@@ -0,0 +1,68 @@
+//========== STRUCTURE DATA
+const Constants = require("./../util/constants.js")
+
+//========== PACKAGE
+const { EventEmitter } = require("node:events")
+const axios = require('axios')
+const WebSocket = require("ws");
+
+//========= CLASS
+class Client extends EventEmitter {
+  constructor(options = {}) {
+    super()
+
+    this.token = options?.token || null;
+  }
+
+  login(token) {
+    if (this.token === null) {
+      if (!token) throw new Error("Token Tidak Ada")
+    }
+    this.startWebsocket()
+  }
+
+  destroy() {
+    return this.ws.destroy()
+  }
+
+  startWebsocket() {
+    let wssurl = `wss://ws.revolt.chat?version=1&format=json`
+ 
+    this.ws = new WebSocket(wssurl);
+    this.ws.onopen = () => {
+      this.ws.sendWebsocket("Authenticate", {token: this.token})
+      setInterval(function(){
+        this.ws.sendWebsocket("Ping", {data:0})
+      },12000)
+      console.log('Lumine.js Succesfull To Connect Websocket');
+    }
+    this.ws.onclose = this.ws.onerror = (e) => {
+      this.ws = null
+      console.log('Reconnect...')
+      this.startWebsocket()
+    }
+    this.ws.onmessage = ({ data }) => {
+      let packet = JSON.parse(data)
+
+      switch (packet.type) {
+        case "Ready":
+          console.log(packet)
+        break;
+      }
+      
+    };
+  }
+
+  sendWebsocket(type, data) {
+    var lostnest = data || {}
+    lostnest.type = type
+    this.ws.send(JSON.stringify(lostnest));
+  }
+
+  requestAPI(method = "", params = "", data) {
+    
+  }
+
+}
+
+module.exports = Client
